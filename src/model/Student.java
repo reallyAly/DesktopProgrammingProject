0e8903887abe12167f22098f4f6d9d072427ee81
Alysson Victor <alyssonsantos@alunos.utfpr.edu.br>
2022-04-16 12:17:59 -0300

    [model] Add RA attribute on Student model class

--- src/model/Student.java
+++ src/model/Student.java
@@ -10,8 +10,17 @@ public class Student extends Person {
     
     public static final String FILENAME = "Students";
 
+    private String RA;
+
     public Student() {
-        
+        this.RA = "";
     }
- 
+
+    public String getRA(){
+        return this.RA;
+    }
+
+    public void setRA(String RA){
+        this.RA = RA;
+    }
 }
