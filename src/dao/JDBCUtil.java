a1d00370a252cf9e7986eec414de1e2a5e33d4a0
Alysson Victor <alysson@pop-os.localdomain>
2022-06-04 20:38:42 -0300

    [db] Add Util class to interact with JDBC

---
+++ src/dao/JDBCUtil.java
@@ -0,0 +1,122 @@
+package dao;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.util.Properties;
+
+/**
+ * A simple data source for getting database connections.
+ */
+public class JDBCUtil {
+
+    private static String url;
+    private static String username;
+    private static String password;
+
+    /**
+     * Initializes the data source.
+     *
+     * @param fileName the name of the property file that contains the database
+     * driver, URL, username, and password
+     * @throws java.io.IOException
+     * @throws java.lang.ClassNotFoundException
+     */
+    public static void init(File fileName) throws IOException, ClassNotFoundException {
+
+        Properties props = new Properties();
+        FileInputStream in = new FileInputStream(fileName);
+        props.load(in);
+        
+        String driver = props.getProperty("mysql.driver");
+        
+        url = props.getProperty("mysql.url");
+        
+        username = props.getProperty("mysql.username");
+        if (username == null) {
+            username = "";
+        }
+        password = props.getProperty("mysql.password");
+        if (password == null) {
+            password = "";
+        }
+        if (driver != null) {
+            Class.forName(driver);
+        }
+    }
+
+    /**
+     * Gets a connection to the database.
+     *
+     * @return the database connection
+     *
+     * @throws java.sql.SQLException
+     */
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    public static boolean next(ResultSet rsdados) {
+        try {
+            if (rsdados != null) {
+                if (!rsdados.isLast()) {
+                    rsdados.next();
+                    return true;
+                }               
+            }
+        } catch (SQLException erro) {
+            System.out.println(erro);
+            return false;
+        }
+        return false;
+    }
+
+    public static boolean previous(ResultSet rsdados) {
+        try {
+            if (rsdados != null) {
+                if (!rsdados.isFirst()) {
+                    rsdados.previous();
+                    return true;
+                }
+            }
+        } catch (SQLException erro) {
+            System.out.println(erro);
+            return false;
+        }
+        return false;
+    }
+
+    public static boolean last(ResultSet rsdados) {
+        try {
+            if (rsdados != null) {
+                if (!rsdados.isLast()) {
+                    rsdados.last();
+                    return true;
+                }
+            }
+        } catch (SQLException erro) {
+            System.out.println(erro);
+            return false;
+        }
+        return false;
+    }
+
+    public static boolean first(ResultSet rsdados) {
+        try {
+            if (rsdados != null) {
+                if (!rsdados.isFirst()) {
+                    rsdados.first();
+                    return true;
+                }
+            }
+        } catch (SQLException erro) {
+            System.out.println(erro);
+            return false;
+        }
+        return false;
+    }
+}
