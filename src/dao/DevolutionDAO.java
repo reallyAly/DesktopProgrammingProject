8c76e80e5dd351766503d4568065d08829a41eea
Alysson Victor <alysson@pop-os.localdomain>
2022-06-05 02:04:44 -0300

    [db] Update devolutionDAO

--- src/dao/DevolutionDAO.java
+++ src/dao/DevolutionDAO.java
@@ -115,7 +115,7 @@ public class DevolutionDAO extends DAO<Devolution>{
                 +", "
                 +Devolution.COLUMN_CREATEAD_AT
                 +") "
-                +"VALUES (?, ?, ?)";
+                +"VALUES (?, ?)";
     }
 
     @Override
